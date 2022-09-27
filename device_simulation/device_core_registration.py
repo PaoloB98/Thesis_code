11bd2d0514e00e2c1c5f270a4293d1e6eb65c355
Pa-Paolo <paolobono98@gmail.com>
2022-09-27 10:36:30 +0200

    New script for automatic device registration at the core.

---
+++ device_simulation/device_core_registration.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+f = open('data/device_reg_body.json')
+data = json.load(f)
+
+for i in range(0, 100):
+    print("Iteration " + str(i) + "\n")
+    num_str = ""
+    if (i < 10):
+        num_str = str(0) + str(i)
+    else:
+        num_str = str(i)
+
+    imsi = "imsi-2089300000000" + num_str
+    url = f"http://localhost:5000/api/subscriber/{imsi}/20893"
+    data['ueId'] = imsi
+    headers_c = {"Content-Type": "application/json", "Accept-Charset": "utf-8", "Token": "admin"}
+
+    response = requests.post(url,json=data,headers=headers_c)
+
+    if(response.status_code==201):
+        print(f"Successfully added device {imsi}")
+    else:
+        print(f"Error while adding {imsi}")
